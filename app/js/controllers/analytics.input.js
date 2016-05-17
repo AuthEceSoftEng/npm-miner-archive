2b0387a139e3ab40d7fcc12003442b30508d4c06
mujx <mujx@users.noreply.github.com>
2016-05-17 19:02:58 +0300

    Add option to return to the initial state

--- app/js/controllers/analytics.input.js
+++ app/js/controllers/analytics.input.js
@@ -1,11 +1,17 @@
 'use strict'
 
 const controllersModule = require('./_index')
+const _ = require('lodash')
 
-function AnalyticsInputCtrl ($log, $state, toastr, Gremlin) {
+function AnalyticsInputCtrl ($log, $state, $rootScope, toastr, Gremlin) {
   'ngInject'
 
   this.description = 'Search for any npm package...'
+
+  $rootScope.$on('navbar:clean', () => {
+    this.hideResults = true
+    this.query = undefined
+  })
 
   this.search = (query) => {
     if (!query) {
@@ -15,8 +21,10 @@ function AnalyticsInputCtrl ($log, $state, toastr, Gremlin) {
     Gremlin.searchText(query)
     .then((results) => {
       if (_.isEmpty(results)) {
+        this.hideResults = true
         toastr.info(`No results found for ${query}`)
       } else {
+        this.hideResults = false
         $state.go('main.search.results', { results })
       }
     })
--- app/js/controllers/navbar.js
+++ app/js/controllers/navbar.js
@@ -2,12 +2,17 @@
 
 const controllersModule = require('./_index')
 
-function NavbarCtrl ($log, $rootScope) {
+function NavbarCtrl ($log, $rootScope, $state) {
   'ngInject'
 
   this.proccessQuery = (query) => {
     this.query = ''
     $rootScope.$emit('navbar:search', query)
+  }
+
+  this.clean = () => {
+    $rootScope.$emit('navbar:clean')
+    $state.go('main.search')
   }
 }
 
